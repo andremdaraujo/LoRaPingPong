e3fc8c655cd977924897fd53b2e1f0b928f3cc86
André Araújo <94403601+andremdaraujo@users.noreply.github.com>
2024-01-18 23:14:36 -0300

    Changed radio frequency to 915MHz.

--- SubGHz_Phy_PingPong/SubGHz_Phy/App/subghz_phy_app.h
+++ SubGHz_Phy_PingPong/SubGHz_Phy/App/subghz_phy_app.h
@@ -41,7 +41,7 @@ extern "C" {
 #define USE_MODEM_LORA  1
 #define USE_MODEM_FSK   0
 
-#define RF_FREQUENCY                                868000000 /* Hz */
+#define RF_FREQUENCY                                915000000 /* Hz */
 
 #ifndef TX_OUTPUT_POWER   /* please, to change this value, redefine it in USER CODE SECTION */
 #define TX_OUTPUT_POWER                             14        /* dBm */
